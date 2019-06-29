144c057eb0871f1209c583ef851dd65002d33ab3
Yoav Levin <yoav.levin@gmail.com>
2019-06-29 09:40:11 +0300

    Fixed the unicode decoder to decode non unicode

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const CSVToJSON = require('csvtojson');
 const JSONToCSV = require('json2csv').parse;
 const FileSystem = require('fs');
-const utf8 = require('utf8');
+const unidecode = require('unidecode');
 
 CSVToJSON().fromFile("./catalog_products.csv").then(source =>{ 
-    //const csv = JSONToCSV(source,{fields: ["sku","title","hardware","price"]});
-    //FileSystem.writeFileSync("./destination.csv", csv);
     var regex = /(<([^>]+)>)/ig;
     for(var i=0;i<source.length;i++){
         source[i].description = source[i].description.replace(regex, "");
         source[i].description = source[i].description.replace("\uABCD", "");
+        //decode uti for invalid chaecters 
+        source[i].collection = unidecode(source[i].collection);
      }
-     console.log(source);
-     const csv = JSONToCSV(source);
+    
+     const csv = JSONToCSV(source,{quote: ''} );
      FileSystem.writeFileSync("./csv_output.csv", csv);
     }
     )
