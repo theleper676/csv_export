464a76c9113e3a2c88cdd03625e1694d78a92272
theleper676 <yoav.levin@gmail.com>
2019-07-15 16:48:58 +0300

    Complete overhaul

--- app.js
+++ app.js
@@ -2,31 +2,20 @@ const CSVToJSON = require('csvtojson');
 const JSONToCSV = require('json2csv').parse;
 const FileSystem = require('fs');
 const unidecode = require('unidecode');
-const striptags = require('striptags');
+const stripHtml = require("string-strip-html");
 
-CSVToJSON().fromFile("./catalog_products.csv").then(source =>{ 
-    var regex = /(<([^>]+)>)/ig;
-    for(var i=0;i<source.length;i++){
-        source[i].description = source[i].description.replace(regex, "");
-        source[i].description = source[i].description.replace("\uABCD", "");
-        //remove html tags from additional Info decription 1
-        source[i].additionalInfoDescription1 = striptags(source[i].additionalInfoDescription1);
 
-        //decode uti for invalid chaecters 
-        source[i].collection = unidecode(source[i].collection);
-        source[i].additionalInfoDescription1 = unidecode(source[i].additionalInfoDescription1);
-        source[i].additionalInfoDescription2 = unidecode(source[i].additionalInfoDescription2);
-     }
+CSVToJSON().fromFile("./catalog_products.csv").then(source => {
 
-     var productDescription3 = source[0].productOptionDescription3;
-     var splitProductDescrption = productDescription3.split(';');
-     if(splitProductDescrption.length >=30){
-         console.log(splitProductDescrption.length);
-     }
+    source.forEach(sourceJson => {
 
-     
-     
-     const csv = JSONToCSV(source,{excelBOM : true});
-     FileSystem.writeFileSync("./csv_output.csv", csv);
-    }
-    )
+        sourceJson.description = stripHtml(sourceJson.description);
+        sourceJson.description = JSON.stringify(sourceJson.description);
+        sourceJson.description = unidecode(sourceJson.description);
+
+
+    });
+    const csv = JSONToCSV(source,);
+    FileSystem.writeFileSync("./csv_output.csv", csv);
+}
+)
