ecaba44407c958b8303822b8af97d757c4d5a07f
theleper676 <yoav.levin@gmail.com>
2019-07-16 18:06:35 +0300

    Better Praseing
    Added Papaparase to better unscape the JSON file, ans delete the quotes for handleID and field so it wont fuck it up

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const JSONToCSV = require('json2csv').parse;
 const FileSystem = require('fs');
 const unidecode = require('unidecode');
 const stripHtml = require("string-strip-html");
+const papaparse = require('papaparse');
+
 
 
 CSVToJSON().fromFile("./catalog_products.csv").then(source => {
@@ -12,10 +14,14 @@ CSVToJSON().fromFile("./catalog_products.csv").then(source => {
         sourceJson.description = stripHtml(sourceJson.description);
         //sourceJson.description = JSON.stringify(sourceJson.description);
         sourceJson.description = unidecode(sourceJson.description);
+        sourceJson.handleId = sourceJson.handleId.replace(/\"/g, "");
+        sourceJson.fieldType = sourceJson.fieldType.replace(/\"/g, "");
+
         
 
     });
-    const csv = JSONToCSV(source);
+    //const csv = JSONToCSV(source);
+    const csv = papaparse.unparse(source,{escapeChar: '"',quotes:false,quoteChar: '"'});
     FileSystem.writeFileSync("./csv_output.csv", csv);
 }
 )
