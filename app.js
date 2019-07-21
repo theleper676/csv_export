449e64774572722803606f87691679a1cf2707cd
theleper676 <yoav.levin@gmail.com>
2019-07-21 11:15:07 +0300

    Handle undecoded Image URI

--- app.js
+++ app.js
@@ -5,53 +5,54 @@ const stripHtml = require("string-strip-html");
 const papaparse = require('papaparse');
 
 
-
-//CSVToJSON().fromFile("./catalog_products.csv").then
-
-CSVToJSON({delimiter:'auto'}).fromFile("./catalog_products.csv").then(source => {
+CSVToJSON({ delimiter: 'auto' }).fromFile("./catalog_products.csv").then(source => {
 
     source.forEach(sourceJson => {
 
-            if(sourceJson.handleId.length == 0){
-                console.log('HnadleId is null');
-                return;
-            }
-            else if(sourceJson.fieldType.length == 0){
-                console.log('fieldType is null');
-                return;
-            }
+        CheckIfIncludesSpaces(sourceJson);
 
-            StripHtml(sourceJson);
-            Unidecode(sourceJson);
-            DeleteHandleIDQuotes(sourceJson);
-            URIencoding(sourceJson);
-        
+        if (sourceJson.handleId.length == 0) {
+            console.log('HnadleId is null');
+            return;
+        }
+        else if (sourceJson.fieldType.length == 0) {
+            console.log('fieldType is null');
+            return;
+        }
+
+        StripHtml(sourceJson);
+        Unidecode(sourceJson);
+        DeleteHandleIDQuotes(sourceJson);
+
         sourceJson.fieldType = sourceJson.fieldType.replace(/\"/g, "");
     });
 
     //Stip any HTML tags from description 
-    function StripHtml (sourceJson){
+    function StripHtml(sourceJson) {
         sourceJson.description = stripHtml(sourceJson.description);
     }
 
     //Unidcode Description and name
-    function Unidecode (sourceJson){
+    function Unidecode(sourceJson) {
         sourceJson.description = unidecode(sourceJson.description);
-       //sourceJson.name = unidecode(sourceJson.name);
         sourceJson.collection = unidecode(sourceJson.collection);
         sourceJson.additionalInfoTitle1 = unidecode(sourceJson.additionalInfoTitle1);
-        sourceJson.additionalInfoDescription1 = unidecode(sourceJson.additionalInfoDescription1);
+        sourceJson.additionalInfoDescription1 = unidecode(sourceJson.additionalInfoDescription1)
     };
     //Delete Handle ID Quotes
-    function DeleteHandleIDQuotes (sourceJson){
+    function DeleteHandleIDQuotes(sourceJson) {
         sourceJson.handleId = sourceJson.handleId.replace(/\"/g, "");
     };
 
-    function URIencoding (sourceJson){
-        sourceJson.productImageUrl = encodeURI(sourceJson.productImageUrl);
-    };
+    //check if image url has undecoded spaces and decode back. 
+    function CheckIfIncludesSpaces(sourceJson) {
+        if (sourceJson.productImageUrl.includes('%20')) {
+            console.log(sourceJson.name + ' Has uncoded image space');
+            sourceJson.productImageUrl = encodeURI(sourceJson.productImageUrl);
+        }
+    }
 
-    const csv = papaparse.unparse(source,{escapeChar: '"',quotes:false,quoteChar: '"'});
+    const csv = papaparse.unparse(source, { escapeChar: '"', quotes: false, quoteChar: '"' });
     FileSystem.writeFileSync("./csv_output.csv", csv);
 }
 )
